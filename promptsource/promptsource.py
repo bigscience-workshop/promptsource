9cdbf471e2fcc2dd0b4b52594a299d3019be5c96
Stephen Bach <stephenhbach@gmail.com>
2021-05-19 11:31:35 -0400

    First draft.

---
+++ promptsource/promptsource.py
@@ -0,0 +1,136 @@
+import datasets
+import random
+import streamlit as st
+
+import session_state as SessionState
+from templates import Template, TemplateCollection
+
+st.set_page_config(layout="wide")
+st.title('PromptSource: Create Prompt Templates')
+
+#
+# Loads template data
+#
+try:
+    with open("./templates.yaml", 'r') as f:
+        templates = TemplateCollection.read_from_file(f)
+except FileNotFoundError:
+    st.error("Unable to load the templates file!\n\n"
+             "We expect the file templates.yaml to be in the working directory. "
+             "You might need to restart the app in the root directory of the repo.")
+    st.stop()
+
+
+def save_data(message="Done!"):
+    with open("./templates.yaml", 'w') as f:
+        templates.write_to_file(f)
+        st.success(message)
+
+
+#
+# Loads dataset information
+#
+dataset_list = datasets.list_datasets()
+
+#
+# Initializes state
+#
+session_state = SessionState.get(example_index=0, dataset=dataset_list[0])
+
+#
+# Select a dataset
+#
+# TODO: Currently raises an error if you select a dataset that requires a
+# TODO: configuration. Not clear how to query for these options.
+dataset_key = st.sidebar.selectbox('Dataset', dataset_list, key='dataset_select',
+                 help='Select the dataset to work on. Number in parens ' +
+                      'is the number of prompts created.')
+st.sidebar.write("HINT: Try ag_news or trec for examples.")
+
+#
+# If a particular dataset is selected, loads dataset and template information
+#
+if dataset_key is not None:
+    dataset = datasets.load_dataset(dataset_key)
+
+    st.sidebar.subheader("Dataset Info")
+    st.sidebar.markdown(f"[Hugging Face Page](https://huggingface.co/datasets/{dataset_key})")
+
+    with st.form("example_form"):
+        st.sidebar.subheader("Random Training Example")
+        new_example_button = st.sidebar.button("New Example", key="new_example")
+        if new_example_button or dataset_key != session_state.dataset:
+            session_state.example_index = random.randint(0, len(dataset['train']))
+            session_state.dataset = dataset_key
+        example = dataset['train'][session_state.example_index]
+        st.sidebar.write(example)
+
+    col1, _, col2 = st.beta_columns([18, 1, 6])
+
+    with col1:
+        with st.beta_expander("Select Template", expanded=True):
+            with st.form("new_template_form"):
+                new_template_input = st.text_input("New Template Name", key="new_template_key", value="",
+                                                   help="Enter name and hit enter to create a new template.")
+                new_template_submitted = st.form_submit_button("Create")
+                if new_template_submitted:
+                    new_template_name = new_template_input
+                    if new_template_name in templates.get_templates(dataset_key):
+                        st.error(f"A template with the name {new_template_name} already exists "
+                                 f"for dataset {dataset_key}.")
+                    else:
+                        template = Template(new_template_name, 'return ""', 'return ""', 'return ""', "")
+                        templates.add_template(dataset_key, template)
+                        save_data()
+                else:
+                    new_template_name = None
+
+            dataset_templates = templates.get_templates(dataset_key)
+            template_list = list(dataset_templates.keys())
+            if new_template_name:
+                index = template_list.index(new_template_name)
+            else:
+                index = 0
+            template_name = st.selectbox('', template_list, key='template_select',
+                                          index=index, help='Select the template to work on.')
+
+            if st.button("Delete Template", key="delete_template"):
+                templates.remove_template(dataset_key, template.get_name())
+                save_data("Template deleted!")
+
+        #
+        # If template is selected, displays template editor
+        #
+        if template_name is not None:
+            with st.form("edit_template_form"):
+                template = dataset_templates[template_name]
+
+                code_height = 40
+                input_fn_code = st.text_area('Input Function', height=code_height, value=template.input_fn)
+                prompt_fn_code = st.text_area('Prompt Function', height=code_height, value=template.prompt_fn)
+                output_fn_code = st.text_area('Output Function', height=code_height, value=template.output_fn)
+
+                reference = st.text_area("Template Reference",
+                                         help="Your name and/or paper reference.",
+                                         value=template.reference)
+
+                if st.form_submit_button("Save"):
+                    template.input_fn = input_fn_code
+                    template.prompt_fn = prompt_fn_code
+                    template.output_fn = output_fn_code
+                    template.reference = reference
+                    save_data()
+
+    #
+    # Displays template output on current example if a template is selected
+    # (in second column)
+    #
+    with col2:
+        if template_name is not None:
+            st.empty()
+            st.subheader("Template Output")
+            template = dataset_templates[template_name]
+            prompt = template.apply(example)
+            st.write(prompt[0])
+            st.write(prompt[1])
+            st.write(prompt[2])
---
+++ promptsource/templates.py
@@ -0,0 +1,202 @@
+import yaml
+
+
+def get_sample_template_data():
+    data = TemplateCollection()
+
+    ag_news_template = Template(
+        'basic',
+        'return example["text"]',
+        'return "Is this an example of news about world politics, sports, business, or technology?"',
+        'label_map = {\n'
+        '    0: "World politics",\n'
+        '    1: "Sports",\n'
+        '    2: "Business",\n'
+        '    3: "Technology"}\n'
+        'return label_map[example["label"]]',
+        'Example template.')
+
+    data.add_template("ag_news", ag_news_template)
+
+    trec_template = Template(
+        'basic',
+        'return example["text"]',
+        'return "Is this asking about a description, an entity, '
+        'an abbreviation, a person, or a quantity?"',
+        'label_map = {\n'
+        '    0: "A description",\n'
+        '    1: "An entity",\n'
+        '    2: "An abbreviation",\n'
+        '    3: "A person",\n'
+        '    4: "A quantity"}\n'
+        'return label_map[example["label-coarse"]]',
+        'Example template.')
+
+    data.add_template("trec", trec_template)
+
+    return data
+
+
+class TemplateCollection:
+    """
+    Collection of prompt templates.
+
+    Templates are organized by dataset. The key for a dataset is either the
+    dataset name or, f the dataset requires a configuration, the key is a
+    tuple of (dataset name, configuration name).
+    """
+
+    def __init__(self):
+        """
+        Initializes an empty template collection.
+        """
+        # Templates are organized as a dictionary of dictionaries.
+        # Outer dictionary is keyed by dataset key.
+        # Inner dictionary is keyed by template name.
+        self.templates = {}
+
+    @classmethod
+    def read_from_file(cls, file):
+        """
+        Reads a file containing a prompt collection.
+
+        :param file: file-like object producing strings
+        """
+        template_dict = yaml.load(file)
+        templates = TemplateCollection()
+        templates.templates = template_dict
+        return templates
+
+    def write_to_file(self, file):
+        """
+        Writes to a file with the current prompt collection.
+
+        :param file: file-like object supporting string inputs
+        """
+        yaml.dump(self.templates, file)
+
+    def add_template(self, dataset, template):
+        """
+        Adds a new template for the dataset
+
+        :param dataset: dataset key for the template
+        :param template: template
+        """
+        if dataset not in self.templates:
+            self.templates[dataset] = {}
+        self.templates[dataset][template.get_name()] = template
+
+    def remove_template(self, dataset, template_name):
+        """
+        Deletes a template
+
+        :param dataset: dataset key for the template
+        :param template_name: name of template to remove
+        """
+        if dataset not in self.templates:
+            raise ValueError("No templates for dataset exist.")
+
+        if template_name not in self.templates[dataset]:
+            raise ValueError(f"No template with name {template_name} " +
+                             f"for dataset {dataset} exists.")
+
+        del self.templates[dataset][template_name]
+
+        if len(self.templates[dataset]) == 0:
+            del self.templates[dataset]
+
+    def get_templates(self, dataset):
+        """
+        Returns all templates for a dataset
+
+        :param dataset: dataset key
+        :return: copy of internal dictionary with template names as keys
+        """
+        if dataset not in self.templates:
+            return {}
+
+        return self.templates[dataset].copy()
+
+    def __len__(self):
+        size = 0
+        for dataset in self.templates.keys():
+            size += len(self.templates[dataset])
+        return size
+
+
+class Template(yaml.YAMLObject):
+    """
+    A prompt template.
+    """
+    yaml_tag = u'!Template'
+
+    def __init__(self, name, input_fn, prompt_fn, output_fn, reference):
+        """
+        Creates a prompt template.
+
+        A prompt template is made up three main pieces: strings that define
+        three functions, one each for generating the input, the prompt, and the
+        output given an example. These strings should not include the function
+        signature, but should assume that there is an input called "example".
+        Each function should return a string.
+
+        :param name: unique name (per dataset) for template
+        :param input_fn: string defining function that creates input from example
+        :param prompt_fn: string defining function that creates prompt from example
+        :param output_fn: string defining function that creates output from example
+        :param reference: string metadata describing author or paper reference
+                          for template
+        """
+        self.name = name
+        self.input_fn = input_fn
+        self.prompt_fn = prompt_fn
+        self.output_fn = output_fn
+        self.reference = reference
+
+    def get_name(self):
+        """
+        Returns the name of the template
+
+        :return: unique (per dataset) name for template
+        """
+        return self.name
+
+    def get_reference(self):
+        """
+        Returns the bibliographic reference (or author) for the template
+
+        :return: reference as a string
+        """
+        return self.reference
+
+    def apply(self, example):
+        """
+        Creates a prompt by applying this template to an example
+
+        :param example: the dataset example to create a prompt for
+        :return: tuple of 3 strings, for input, prompt, and output
+        """
+        fns = {}
+        exec(self._make_fun_str("input_fn", ["example"], self.input_fn), fns)
+        exec(self._make_fun_str("prompt_fn", ["example"], self.prompt_fn), fns)
+        exec(self._make_fun_str("output_fn", ["example"], self.output_fn), fns)
+        return (fns['input_fn'](example),
+                fns['prompt_fn'](example),
+                fns['output_fn'](example))
+
+    @classmethod
+    def _make_fun_str(cls, name, args, body):
+        """
+        Creates a string representation of a Python function.
+
+        :param name: the name of the function
+        :param args: iterable of strings naming function arguments
+        :param body: the function definition. The outermost context should be unindented.
+        :return: full function definition that can be parsed by exec
+        """
+        arg_str = ", ".join(args)
+        signature = f"def {name}({arg_str}):\n"
+        body = "\n".join([("    " + line) for line in body.split("\n")])
+        return signature + body
+
+
