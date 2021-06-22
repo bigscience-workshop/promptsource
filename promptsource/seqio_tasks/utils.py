25369e3cdb609618dd3c790ae8d76e462df1cf11
Colin Raffel <craffel@gmail.com>
2021-06-22 13:59:23 -0400

    Fix column removal logic

--- promptsource/seqio_tasks/utils.py
+++ promptsource/seqio_tasks/utils.py
@@ -51,7 +51,10 @@ def apply_template(dataset, template):
     def filter_fn(ex):
         return (len(ex["inputs"]) > 0 and len(ex["targets"]) > 0)
 
-    return dataset.map(map_fn, remove_columns=dataset.column_names).filter(filter_fn)
+    original_columns = dataset.column_names
+    dataset = dataset.map(map_fn).filter(filter_fn)
+    # map keeps original columns, remove them
+    return dataset.remove_columns(set(original_columns) - {"inputs", "targets"})
 
 
 def get_dataset_splits(dataset_name, subset_name=None):
