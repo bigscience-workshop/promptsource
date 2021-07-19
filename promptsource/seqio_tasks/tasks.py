40ae2b5e3c4734a614f9934a35d2fc923f381fb1
Albert Webson <awebson@cs.brown.edu>
2021-07-19 18:45:04 -0400

    revert seqio.mixing_rate_num_examples to 1 million

--- promptsource/seqio_tasks/tasks.py
+++ promptsource/seqio_tasks/tasks.py
@@ -150,22 +150,22 @@ def clean(prompt):
     return True
 
 
-tasks = list(filter(clean, all_tasks))
-CLEAN_TASKS = [t for t in tasks if not t["skip_train"]]
+filtered_tasks = list(filter(clean, all_tasks))
+CLEAN_TASKS = [t for t in filtered_tasks if not t["skip_train"]]
 
 
 seqio.MixtureRegistry.add(
     "clean_tasks",
     [task for task in CLEAN_TASKS if task not in TASK_BLACKLIST],
-    default_rate=functools.partial(seqio.mixing_rate_num_examples, maximum=500_000),
+    default_rate=functools.partial(seqio.mixing_rate_num_examples, maximum=1000000),
 )
 
 # Tasks to evaluate models trained on clean_tasks
-CLEAN_EVAL_TASKS = [t for t in tasks if t["do_eval"]]
+CLEAN_EVAL_TASKS = [t for t in filtered_tasks if t["do_eval"]]
 
 
 seqio.MixtureRegistry.add(
     "clean_eval_tasks",
     [task for task in CLEAN_EVAL_TASKS if task not in TASK_BLACKLIST],
-    default_rate=functools.partial(seqio.mixing_rate_num_examples, maximum=500_000),
+    default_rate=functools.partial(seqio.mixing_rate_num_examples, maximum=1000000),
 )
