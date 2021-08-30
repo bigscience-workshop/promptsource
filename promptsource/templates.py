1b80c664c0ae7f21ae31a44a3f62925acaf7793b
Stephen Bach <stephenhbach@gmail.com>
2021-08-30 12:50:32 -0400

    Update new metadata from CSV.

--- promptsource/templates.py
+++ promptsource/templates.py
@@ -183,19 +183,7 @@ class Template(yaml.YAMLObject):
             self,
             task_format: str,  # dropdown choice of 'Classification', 'Generation', 'Extraction'
             task_template: bool,
-
-            nontrivial_choices_given,
-            nontrivial_choices_hidden,
-            trivial_choices_given,
-            trivial_choices_hidden,
-            negated_answers,
-            counting,
-            long_distance,
-            no_sep_2_sentences,
-            answer_span_indices,
-            non_natural_language,
-
-            metric: Optional[str] = None,
+            metric: Optional[List[str]] = None,
 
             # if task_format == classification
             choices_in_prompt: Optional[bool] = None,
@@ -211,46 +199,24 @@ class Template(yaml.YAMLObject):
             etc. and nontrivial choices are other types of choices denoted in
             the answer_choices field.
 
-            :param task_template: If True, this template corresponds to the original
-                        or usual task for this dataset
-            :param nontrivial_choices_given: If True, nontrivial choices are
-                        included in in the prompt
-            :param nontrivial_choices_hidden: If True, the template requires
-                        nontrivial choices but they are not included in the prompt
-            :param trivial_choices_given: If True, trivial choices are
-                        included in in the prompt
-            :param trivial_choices_hidden: If True, the template requires
-                        trivial choices but they are not included in the promp
-            :param negated_answers: If True, the template uses negation to flip
-                        the usual answer for the task
-            :param counting: If True, the template requires counting of some kind
-            :param long_distance: If True, the description of the task in the prompt
-                        is usually separated by a gap of more than 2 paragraphs
-            :param no_sep_2_sentences: If True, the template places multiple fields
-                        of the example contiguously, with no explicit separator,
-                        in a way that might create ambiguity
-            :param answer_span_indices: If True, the template refers to choices
-                        with (and requires a response with respect to) indices
-            :param non_natural_language: If True, the template requires generating
-                        anything other than natural language, such as code,
-                        complex math expressions, or expert linguistic annotations
-                        like parse trees.
+            :param task_format: Either "Classification" (output is from a set of labels),
+                "Generation" (output is open ended and not contained in the prompt),
+                or "Extraction" (output is open ended but contained in the prompt,
+                including indexed choices)
+            :param task_template: If True, this prompt asks a model to perform the original task designed for
+                this dataset.
+            :param metric: List of strings denoting metrics to use for evaluation
+            :param choices_in_prompt: If True, the answer choices are included in the templates such that models
+                see those choices in the input. Only applicable to classification tasks.
+            :param _do_train: internal parameter. If True, this template should be
+                used for training
+            :param _do_train: internal parameter. If True, this template should be
+                used for evaluation
             """
             self.task_format = task_format
             self.metric = metric
             self.choices_in_prompt = choices_in_prompt
             self.task_template = task_template
-            self.nontrivial_choices_given = nontrivial_choices_given
-            self.nontrivial_choices_hidden = nontrivial_choices_hidden
-            self.trivial_choices_given = trivial_choices_given
-            self.trivial_choices_hidden = trivial_choices_hidden
-            self.negated_answers = negated_answers
-            self.counting = counting
-            self.long_distance = long_distance
-            self.no_sep_2_sentences = no_sep_2_sentences
-            self.answer_span_indices = answer_span_indices
-            self.non_natural_language = non_natural_language
-
             self._do_train = _do_train
             self._do_eval = _do_eval
 
