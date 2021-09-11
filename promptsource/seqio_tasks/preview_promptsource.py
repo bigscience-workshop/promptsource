dd1417f47f9b5290c7a184600ca9974268871d57
Albert Webson <awebson@cs.brown.edu>
2021-09-11 11:49:13 -0400

    loading train/eval mixtures from Google Sheet

---
+++ promptsource/seqio_tasks/preview_promptsource.py
@@ -0,0 +1,78 @@
+import csv
+from typing import Dict, List
+import pkg_resources
+
+from rich import inspect
+from rich.pretty import pprint
+# from t5.data.glue_utils import get_glue_metric, get_super_glue_metric
+# from t5.evaluation.metrics import accuracy, mean_multiclass_f1, rouge
+
+from promptsource.templates import TemplateCollection
+
+
+# NON_GLUE_METRICS = {  # for those with do_eval = True
+#     "anli": [accuracy],
+#     "hans": [accuracy],
+#     "circa_goldstandard1_judgement": [mean_multiclass_f1(num_classes=8), accuracy],
+#     "circa_goldstandard2_judgement": [mean_multiclass_f1(num_classes=5), accuracy],
+#     "mc_taco": [accuracy],
+#     "nq_open": [accuracy],
+#     "qa_srl": [accuracy],
+#     "openbookqa": [accuracy],
+#     "race": [accuracy],
+#     "social_i_qa": [accuracy],
+#     "emo": [mean_multiclass_f1(num_classes=4)],
+#     "xsum": [rouge],
+# }
+
+
+def preview() -> None:
+    experiment_path = pkg_resources.resource_filename(__name__, "experiment_D4.csv")
+
+    train_sets = []
+    eval_sets = []
+    train_set_names = []
+    eval_set_names = []
+    with open(experiment_path) as exp_file:
+        reader = csv.DictReader(exp_file)
+        for row in reader:
+            if row['skip']:
+                continue
+            if row['subset'] == '':
+                row['subset'] = None  # to match promptsource.Template object
+            if row['do_train'] == 'TRUE':
+                train_sets.append(row)
+                train_set_names.append((row['HF_name'], row['subset']))
+            if row['do_eval'] == 'TRUE':
+                eval_sets.append(row)
+                eval_set_names.append((row['HF_name'], row['subset']))
+
+        # print(f'Number of non-desk-rejected datasets = {len(all_datasets)}')
+
+    D4_names = train_set_names + eval_set_names
+    print(f'Number of training sets = {len(train_sets)}')
+    print(f'Number of evaluation sets = {len(eval_sets)}')
+
+    template_collection = TemplateCollection()
+    for dataset_name, subset in template_collection.keys:
+        if (dataset_name, subset) not in train_set_names:  # D4_names:
+            continue
+        OG = 0
+        non_OG = 0
+        dataset = template_collection.get_dataset(dataset_name, subset)
+        for template_name in dataset.all_template_names:
+            template = dataset[template_name]
+            # if dataset_name == 'ropes':
+            #     inspect(template.metadata)
+            if template.metadata.original_task is True:
+                OG += 1
+            elif template.metadata.original_task is False:
+                non_OG += 1
+            elif template.metadata.original_task is None:
+                print(dataset_name, 'has not flagged original tasks')
+                continue
+        print(dataset_name, subset, OG, non_OG)
+
+
+if __name__ == "__main__":
+    preview()
