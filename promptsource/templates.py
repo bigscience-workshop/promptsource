c861f0acf8ae7991a69edbea8b3b4a796b9f79eb
KhalidAlt <ekh@Protonmail.com>
2022-05-09 19:59:18 +0300

    fix issues

--- promptsource/templates.py
+++ promptsource/templates.py
@@ -26,7 +26,7 @@ env.globals.update(zip=zip)
 
 # These are users whose datasets should be included in the results returned by
 # filter_english_datasets (regardless of their metadata)
-INCLUDED_USERS = {"Zaid", "craffel","khalidalt"}
+INCLUDED_USERS = {"Zaid", "craffel", "GEM", "khalidalt"}
 
 
 def highlight(input):
