5e0045aa0228f095be7f471a4a07d126ca641878
Shanya Sharma - s0s0cr3 <Shanya.Sharma@walmartlabs.com>
2022-05-10 23:17:30 +0530

    adding crd3 prompts

--- promptsource/templates.py
+++ promptsource/templates.py
@@ -26,7 +26,7 @@ env.globals.update(zip=zip)
 
 # These are users whose datasets should be included in the results returned by
 # filter_english_datasets (regardless of their metadata)
-INCLUDED_USERS = {"Zaid", "craffel", "GEM"}
+INCLUDED_USERS = {"Zaid", "craffel", "shanya"}
 
 
 def highlight(input):
