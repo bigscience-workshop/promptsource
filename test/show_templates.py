65cff1408037e275a8284f21907c785d8c32f498
Victor SANH <victorsanh@gmail.com>
2022-06-25 16:16:33 -0400

    allow `test/show_templates` on community datasets (#777)

--- test/show_templates.py
+++ test/show_templates.py
@@ -15,71 +15,72 @@ if "templates.yaml" not in args.dataset_path:
 path = args.dataset_path.split("/")
 
 if path[2] in INCLUDED_USERS:
-    print("Skipping showing templates for community dataset.")
+    dataset_name = f"{path[2]}/{path[3]}"
+    subset_name = path[4] if len(path) == 6 else ""
 else:
     dataset_name = path[2]
     subset_name = path[3] if len(path) == 5 else ""
 
-    template_collection = TemplateCollection()
+template_collection = TemplateCollection()
 
-    dataset = get_dataset(dataset_name, subset_name)
-    splits = list(dataset.keys())
+dataset = get_dataset(dataset_name, subset_name)
+splits = list(dataset.keys())
 
-    dataset_templates = template_collection.get_dataset(dataset_name, subset_name)
-    template_list = dataset_templates.all_template_names
+dataset_templates = template_collection.get_dataset(dataset_name, subset_name)
+template_list = dataset_templates.all_template_names
 
-    width = 80
-    print("DATASET ", args.dataset_path)
+width = 80
+print("DATASET ", args.dataset_path)
 
-    # First show all the templates.
-    for template_name in template_list:
-        template = dataset_templates[template_name]
-        print("TEMPLATE")
-        print("NAME:", template_name)
-        print("Is Original Task: ", template.metadata.original_task)
-        print(template.jinja)
-        print()
+# First show all the templates.
+for template_name in template_list:
+    template = dataset_templates[template_name]
+    print("TEMPLATE")
+    print("NAME:", template_name)
+    print("Is Original Task: ", template.metadata.original_task)
+    print(template.jinja)
+    print()
 
-    # Show examples of the templates.
-    for template_name in template_list:
-        template = dataset_templates[template_name]
-        print()
-        print("TEMPLATE")
-        print("NAME:", template_name)
-        print("REFERENCE:", template.reference)
-        print("--------")
-        print()
-        print(template.jinja)
-        print()
+# Show examples of the templates.
+for template_name in template_list:
+    template = dataset_templates[template_name]
+    print()
+    print("TEMPLATE")
+    print("NAME:", template_name)
+    print("REFERENCE:", template.reference)
+    print("--------")
+    print()
+    print(template.jinja)
+    print()
 
-        for split_name in splits:
-            dataset_split = dataset[split_name]
+    for split_name in splits:
+        dataset_split = dataset[split_name]
 
-            print_counter = 0
-            for example in dataset_split:
-                print("\t--------")
-                print("\tSplit ", split_name)
-                print("\tExample ", example)
-                print("\t--------")
-                output = template.apply(example)
-                if output == [""]:
-                    print("\t Blank result")
-                    continue
+        print_counter = 0
+        for example in dataset_split:
+            print("\t--------")
+            print("\tSplit ", split_name)
+            print("\tExample ", example)
+            print("\t--------")
+            output = template.apply(example)
+            if output == [""]:
+                print("\t Blank result")
+                continue
 
-                xp, yp = output
-                print()
-                print("\tPrompt | X")
-                for line in textwrap.wrap(xp, width=width, replace_whitespace=False):
+            xp, yp = output
+            print()
+            print("\tPrompt | X")
+            for line in textwrap.wrap(xp, width=width, replace_whitespace=False):
+                print("\t", line.replace("\n", "\n\t"))
+            print()
+            print("\tY")
+            for i, y in enumerate(yp):
+                if i > 0:
+                    print()
+                    print("\tY")
+                for line in textwrap.wrap(y, width=width, replace_whitespace=False):
                     print("\t", line.replace("\n", "\n\t"))
-                print()
-                print("\tY")
-                for i, y in enumerate(yp):
-                    if i > 0:
-                        print()
-                        print("\tY")
-                    for line in textwrap.wrap(y, width=width, replace_whitespace=False):
-                        print("\t", line.replace("\n", "\n\t"))
 
-                print_counter += 1
-                if print_counter >= 10:
-                    break
+            print_counter += 1
+            if print_counter >= 10:
+                break
