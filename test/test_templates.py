62ee1a8683496f0cfbcf03ad950b7f2193cea011
Victor Sanh <victorsanh@gmail.com>
2022-07-18 10:29:54 +0000

    de

--- test/test_templates.py
+++ test/test_templates.py
@@ -29,6 +29,7 @@ def test_uuids():
 
             uuid = template.get_id()
             print(dataset_name, subset_name, uuid)
+            assert uuid not in all_uuids
 
             if uuid in all_uuids:
                 raise ValueError(f"Template {template_name} for dataset {dataset_name}/{subset_name} "
