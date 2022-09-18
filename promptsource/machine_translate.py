ff3eada8a736a016e0e2eb56c7424c194c2f2ca7
Muennighoff <n.muennighoff@gmail.com>
2022-09-18 11:41:33 +0200

    XCOPA Machine translated

--- promptsource/machine_translate.py
+++ promptsource/machine_translate.py
@@ -4,6 +4,8 @@ import re
 from promptsource.templates import Template, TemplateCollection
 
 
+### XNLI
+
 PROMPTS = [
     "GPT-3 style",
     "can we infer",
@@ -22,6 +24,34 @@ LANGS = [
     "vi",
     "zh",
 ]
+
+SOURCE_DATASET = TARGET_DATASET = "xnli"
+SOURCE_LANG = "en"
+
+
+### XCOPA
+
+PROMPTS = [
+    "best_option",
+    'C1 or C2? premise, so/because…',
+    "i_am_hesitating",
+    "cause_effect",
+    "plausible_alternatives",
+]
+
+LANGS = [
+#    "id",
+    "sw",
+    "ta",
+    "vi",
+    "zh",
+]
+
+SOURCE_DATASET = "super_glue/copa"
+SOURCE_LANG = None
+TARGET_DATASET = "xcopa"
+
+
 # Path to key
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/niklasmuennighoff/Desktop/gcp_translate_key.json"
 
@@ -41,16 +71,17 @@ def translate(target, text):
         text = text.decode("utf-8")
     # Text can also be a sequence of strings, in which case this method
     # will return a sequence of results for each text.
-    result = translate_client.translate(text, target_language=target)
+    result = translate_client.translate(text, source_language="en", target_language=target, format_="text")
     print("Text: {}".format(result["input"]))
     print("Translation: {}".format(result["translatedText"]))
-    print("Detected source language: {}".format(result["detectedSourceLanguage"]))
+    # If not providing source_language
+    # print("Detected source language: {}".format(result["detectedSourceLanguage"]))
     return result["translatedText"]
 
 
 def normalize_string(zh_string, en_string):
     """
-    This is not specific to zh just to given an example.
+    This is not specific to zh just to give an example & help Codex understand it :-)
     Replaces the content in brackets in zh_string with the content in brackets from en_string.
     All else is left the same in zh_string.
     Args:
@@ -60,11 +91,12 @@ def normalize_string(zh_string, en_string):
         zh_string_normalized: {{premise}} 问题：{{hypothesis}} 对、错或两者都不是？ ||| {{ answer_choices[label] }}
     """
     zh_string_normalized = zh_string
-    en_string_normalized = en_string
     # Find all the content in brackets in zh_string
-    zh_bracket_content = re.findall(r"{{(.*?)}}", zh_string)
+    # For only double brackets {{(.*?)}}, but we do single brackets as well
+    zh_bracket_content = re.findall(r"{(.*?)}", zh_string)
     # Find all the content in brackets in en_string
-    en_bracket_content = re.findall(r"{{(.*?)}}", en_string)
+    # For only double brackets {{(.*?)}}, but we do single brackets as well
+    en_bracket_content = re.findall(r"{(.*?)}", en_string)
     # Replace the content in brackets in zh_string with the content in brackets from en_string
     for i in range(len(zh_bracket_content)):
         zh_string_normalized = zh_string_normalized.replace(zh_bracket_content[i], en_bracket_content[i])
@@ -72,10 +104,10 @@ def normalize_string(zh_string, en_string):
 
 
 template_collection = TemplateCollection()
-source_templates = template_collection.get_dataset("xnli", "en")
+source_templates = template_collection.get_dataset(SOURCE_DATASET, SOURCE_LANG)
 
 for lang in LANGS:
-    target_templates = template_collection.get_dataset("xnli", lang)
+    target_templates = template_collection.get_dataset(TARGET_DATASET, lang)
     for uid, template in source_templates.templates.items():
         if template.name.strip() not in PROMPTS:
             continue
@@ -84,7 +116,7 @@ for lang in LANGS:
         if template.answer_choices is not None:
             choices = template.answer_choices.split("|||")
             for c in choices:
-                answer_choices.append(translate(lang, c.strip()))
+                answer_choices.append(normalize_string(translate(lang, c.strip()), c.strip()))
         or_jinja = template.jinja.strip()
         jinja = normalize_string(translate(lang, or_jinja), or_jinja)
         template_name = template.name.strip() + f"_{lang}mt"
